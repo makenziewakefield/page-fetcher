683a03be063ce1df40f10a0c53c4bc7416e61d4f
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-18 16:59:54 -0600

    added stretch edge cases

--- fetcher.js
+++ fetcher.js
@@ -3,10 +3,16 @@ const fs = require('fs');
 
 const fetcher = (url, filePath) => {
   request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
+    if (error) {
+      console.error('Error fetching data: ', error);
+    } else if (response.statusCode !== 200) {
+      console.error('Unexpected status code: response.statusCode');
+    } else {
       fs.writeFile(filePath, body, (err) => {
-        if (!err) {
-          const fileSize = Buffer.byteLength(body, 'utf8');
+        if (err) {
+          console.error('Error writing to file: ', err);
+        } else {
+          const fileSize = Buffer.byteLength(body, 'utf-8');
           console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
         }
       })
@@ -17,4 +23,10 @@ const fetcher = (url, filePath) => {
 const url = process.argv[2];
 const filePath = process.argv [3];
 
-fetcher(url, filePath);
+if (!url || !filePath) {
+  console.error('Please provide a URL and a file path');
+} else if (fs.existsSync(filePath)) {
+  console.error('File already exists. Please provide a different file path.');
+} else {
+  fetch(url, filePath);
+}
